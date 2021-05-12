e3d1ad552a14bd5d214a0fcfc2d1ed55c1b54521
PushpakB3096 <rtpushpak@gmail.com>
2021-05-12 12:32:04 +0530

    Fetch weather data from API

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import { fetchWeather } from "./api/weather";
 import "./App.css";
 
 const App = () => {
+  // for storing the search term
   const [query, setQuery] = useState("");
+
+  // for storing the weather data
+  const [weather, setWeather] = useState({});
 
   // for clearing the input box
   const clearInput = () => {
     setQuery("");
   };
 
+  // for making the api call
   const search = async (e) => {
+    // only call the API when enter key is pressed
     if (e.key === "Enter") {
       const data = await fetchWeather(query);
 
-      console.log(data);
-      // clear the input after we have the data
+      // setting the weather data to be displayed
+      setWeather(data);
+
+      // clear the input after we have set the data
       clearInput();
     }
   };
