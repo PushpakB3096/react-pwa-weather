0d2ddf26d1f40d77f8e32ee8b954a97b3fc85dbb
PushpakB3096 <rtpushpak@gmail.com>
2021-05-12 12:18:23 +0530

    Add ability to make API call

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ import "./App.css";
 const App = () => {
   const [query, setQuery] = useState("");
 
+  // for clearing the input box
+  const clearInput = () => {
+    setQuery("");
+  };
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      console.log(data);
+      // clear the input after we have the data
+      clearInput();
+    }
+  };
+
   return (
     <div className="main-container">
       <input
@@ -14,6 +29,7 @@ const App = () => {
         placeholder="Enter location..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
       />
     </div>
   );
