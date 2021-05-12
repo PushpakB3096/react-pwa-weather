a30c748e8755a3e07deea38c1421ac4fd002dbf4
PushpakB3096 <rtpushpak@gmail.com>
2021-05-12 11:57:56 +0530

    Add search box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import react from "react";
+import React, { useState } from "react";
 import { fetchWeather } from "./api/weather";
 
 import "./App.css";
 
 const App = () => {
-  return <div className="main-container">App</div>;
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Enter location..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default App;
