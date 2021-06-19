984483db0de547110383865df82140a844786c93
PushpakB3096 <rtpushpak@gmail.com>
2021-06-19 09:08:40 +0530

    Refactor code

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,21 +1,20 @@
-const CACHE_NAME = "version-1";
+const CACHE_NAME = "version-2";
 const urlsToCache = ["index.html", "offline.html"];
 
 const self = this;
 
 // install the service worker
-self.addEventListener("install", (e) => {
-  // loads the given urls from the cache, if cache is available
+self.addEventListener("install", e => {
+  // add all the pages to our local cache
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
 // listen for requests
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e => {
   /*
         For every request that we receive, we need to check if that request is part of the cache.
         If it is, then we are simply 'forwarding' the request and fetching it. While fetching, if
@@ -29,16 +28,16 @@ self.addEventListener("fetch", (e) => {
 });
 
 // activate the service worker
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", e => {
   // caches to keep
   const cacheWhiteList = [];
   cacheWhiteList.push(CACHE_NAME);
 
   // loops through all the caches and removes all that are not present in the whitelist
   e.waitUntil(
-    caches.keys().then((cacheNames) =>
+    caches.keys().then(cacheNames =>
       Promise.all(
-        cacheNames.map((cacheName) => {
+        cacheNames.map(cacheName => {
           if (!cacheWhiteList.includes(cacheName)) {
             return caches.delete(cacheName);
           }
