b9d280d027adcb16ee42c943e8a21db80e1bb297
PushpakB3096 <rtpushpak@gmail.com>
2021-06-19 09:49:36 +0530

    Add caching all requests

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "version-2";
+const CACHE_NAME = "version-1";
 const urlsToCache = ["index.html", "offline.html"];
 
 const self = this;
@@ -20,11 +20,29 @@ self.addEventListener("fetch", e => {
         If it is, then we are simply 'forwarding' the request and fetching it. While fetching, if
         there is an error, then we need to load 'offline.html'
     */
+  //  uncomment below line if you want to only store certain pages in the cache
   e.respondWith(
     caches.match(e.request).then(() => {
       return fetch(e.request).catch(() => caches.match("offline.html"));
     })
   );
+
+  // this will cache all the requests in the cache.
+  // e.respondWith(
+  //   fetch(e.request)
+  //     .then(res => {
+  //       const resClone = res.clone();
+
+  //       // cache any oncoming request
+  //       caches.open(CACHE_NAME).then(cacheName => {
+  //         cacheName.put(e.request, resClone);
+  //       });
+
+  //       // serve the request afterwards
+  //       return res;
+  //     })
+  //     .catch(() => caches.match("offline.html"))
+  // );
 });
 
 // activate the service worker
